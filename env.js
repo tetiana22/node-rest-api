7c1c65cf2b68b1bbcdecf9ce6cf1b4078b518f63
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-03-01 13:16:54 +0000

    create env.js

---
+++ env.js
@@ -0,0 +1,2 @@
+export const DB_HOST =
+  "mongodb+srv://tetiana:04OXGbHoyUjBLIpm@cluster0.6ew1jpa.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
 import mongoose from "mongoose";
+import { DB_HOST } from "./env.js";
 
 mongoose.set("strictQuery", true);
-const DB_HOST =
-  "mongodb+srv://tetiana:04OXGbHoyUjBLIpm@cluster0.6ew1jpa.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
